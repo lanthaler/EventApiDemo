fa5c114d57ffa7011a6379ebbc744934f580d7da
Markus Lanthaler <mark_lanthaler@gmx.net>
2014-07-20 20:32:55 +0200

    Create Hydra CRUD controller for Event entities

---
+++ src/ML/EventApiBundle/Controller/EventController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace ML\EventApiBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use ML\HydraBundle\Controller\HydraController;
+use ML\HydraBundle\Mapping as Hydra;
+use ML\HydraBundle\JsonLdResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use ML\EventApiBundle\Entity\Event;
+
+/**
+ * Event controller
+ *
+ * @Route("/events")
+ */
+class EventController extends HydraController
+{
+    /**
+     * Retrieves all Event entities
+     *
+     * @Route("/", name="event_collection_retrieve")
+     * @Method("GET")
+     *
+     * @Hydra\Operation()
+     * @Hydra\Collection()
+     *
+     * @return array<ML\EventApiBundle\Entity\Event>
+     */
+    public function collectionGetAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('MLEventApiBundle:Event')->findAll();
+
+        return $entities;
+    }
+
+    /**
+     * Creates a new Event entity
+     *
+     * @Route("/", name="event_create")
+     * @Method("POST")
+     *
+     * @Hydra\Operation(
+     *   type = "http://schema.org/AddAction",
+     *   expect = "ML\EventApiBundle\Entity\Event",
+     *   status_codes = {
+     *     "201" = "If the Event entity was created successfully."
+     * })
+     *
+     * @return ML\EventApiBundle\Entity\Event
+     */
+    public function collectionPostAction(Request $request)
+    {
+        $entity = $this->deserialize($request->getContent(), 'ML\EventApiBundle\Entity\Event');
+
+        if (false !== ($errors = $this->validate($entity))) {
+            return $errors;
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($entity);
+        $em->flush();
+
+        $iri = $this->generateUrl('event_retrieve', array('id' => $entity->getId()), true);
+
+        $response = new JsonLdResponse(
+            $this->serialize($entity),
+            201,
+            array('Location' => $iri, 'Content-Location' => $iri)
+        );
+
+        return $response;
+    }
+
+    /**
+     * Retrieves a Event entity
+     *
+     * @Route("/{id}", name="event_retrieve")
+     * @Method("GET")
+     *
+     * @Hydra\Operation()
+     *
+     * @return ML\EventApiBundle\Entity\Event
+     */
+    public function getAction(Event $entity)
+    {
+        return $entity;
+    }
+
+    /**
+     * Replaces an existing Event entity
+     *
+     * @Route("/{id}", name="event_replace")
+     * @Method("PUT")
+     *
+     * @Hydra\Operation(
+     *   type = "http://schema.org/UpdateAction",
+     *   expect = "ML\EventApiBundle\Entity\Event",
+     *   status_codes = {
+     *     "404" = "If the Event entity wasn't found."
+     *   }
+     * )
+     *
+     * @return ML\EventApiBundle\Entity\Event
+     */
+    public function putAction(Request $request, Event $entity)
+    {
+        $entity = $this->deserialize($request->getContent(), $entity);
+
+        if (false !== ($errors = $this->validate($entity))) {
+            return $errors;
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($entity);
+        $em->flush();
+
+        return $entity;
+    }
+
+    /**
+     * Deletes a Event entity
+     *
+     * @Route("/{id}", name="event_delete")
+     * @Method("DELETE")
+     *
+     * @Hydra\Operation(
+     *   type = "http://schema.org/DeleteAction"
+     * )
+     *
+     * @return void
+     */
+    public function deleteAction(Request $request, Event $entity)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($entity);
+        $em->flush();
+    }
+}
---
+++ src/ML/EventApiBundle/Tests/Controller/EventControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ML\EventApiBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Event;
+
+class EventControllerTest extends WebTestCase
+{
+    /*
+    public function testCompleteScenario()
+    {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Retrieve the collection
+        $crawler = $client->request('GET', '/events/');
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        // Retrieve the first member
+        $crawler = $client->click($crawler->selectLink('show')->link());
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+    }
+
+    */
+}
